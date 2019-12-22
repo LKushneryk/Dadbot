1c5ef413091b4a87d6d6e340f121db6846c29148
LKushneryk <38082468+LKushneryk@users.noreply.github.com>
2019-12-22 16:41:03 -0700

    library, regex changes
    swapped out discord.io library for discord.js
    updated regex to include both server-specific and unicode emojis

--- bot.js
+++ bot.js
@@ -1,43 +1,42 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var request = require('request');
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./auth.json');
+const request = require('request');
+const bot = new Discord.Client();
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
+bot.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with 'im' or variants
-    if (user != 'Dadbot') {
-		var regex = /([Ii][’']?\s*[Mm]\s+)([(<@!\d{18}\>)\w\s]+)/;
-		var match = regex.exec(message);
-		// logger.info(message); // for debug only
+    // logger.info(message); // for debug only
+	// logger.info(userID);
+	// logger.info(message.member.level);
+	user = message.author.username;
+	if (user != 'Dadbot' && user != 'Groovy') {
+		var regex = /([Ii][’']?\s*[Mm]\s+)((\w|\s|<:\w+:\d{18}>|(<@!\d{18}>)|(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))+)/;
+		var match = regex.exec(message.content);
 		if (match) {
 			var name = match[2];
 			name = name.trim();
 			if (name != undefined) {
-				bot.sendMessage({
-					to: channelID,
-					message: 'Hi ' + name + ', I\'m Dad!'
-				});
-			}
-					
+				message.channel.send('Hi ' + name + ', I\'m Dad!');
+			}/*
+			serverID = message.guild.id;
+			if (serverID != '442163319424548867') {
+				message.member.setNickname(name);
+			}*/
 		}
 	}
-	if (message == '!dadjoke') {  
+	if (message.content == '!dadjoke') {  
 		let options = {
 			headers: {
 				'User-Agent': 'DadBot'
@@ -46,16 +45,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		};
 		request('https://icanhazdadjoke.com/', options, (err, res, body) => {
 			if (err) {
-			  bot.sendMessage({
-				to: channelID,
-				message: 'error'
-			});
+				message.channel.send('error');
 			}
-			else bot.sendMessage({
-				to: channelID,
-				message: body.joke
-			});
+			else{ 
+				message.channel.send(body.joke);
+			}
 		});
-	}
-	
+	};
 });
+
+bot.login(auth.token);
